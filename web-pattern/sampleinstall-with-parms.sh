50d8a831ad207aab67cf34efa865361fdfb7fc13
Jarek Miszczyk <miszczyk.jaroslaw@mayo.edu>
2015-05-29 16:41:28 -0500

    added cleanup function to remove the app if previously installed

--- web-pattern/sampleinstall-with-parms.sh
+++ web-pattern/sampleinstall-with-parms.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -x
+
+##### Functions
 function usage
 {
     echo "usage: $0 [[[-d wlp_base_dir ] [-r file_repo_uri] [-m mongodb_host]] | [-h]]"
 }
 
-
+function cleanup
+{
+	#Remove the running  server. Don't try to understand the command below, you won't ;-)
+	kill -9 $(ps -ef | grep -i '[S]CREEN -S mongoDBSample' | awk '{print $2}')
+	#Remove the artifacts
+	rm -f $WLP_BASE_DIR/mongoDBSample.jar
+	rm -Rf $WLP_BASE_DIR/wlp/usr/shared/
+	rm -Rf $WLP_BASE_DIR/wlp/usr/servers/mongoDBSample/
+}
+	
 ##### Main
-
 export WLP_BASE_DIR="/opt/was/liberty"
 export FILE_REPO_URI="http://jmchefserver.cloudapp.net:8080/repos"
 export HOSTNAME=`hostname`
@@ -22,7 +32,8 @@ while [ "$1" != "" ]; do
         						FILE_REPO_URI=$1
                                 ;;
         -m | --mongodb_host )   shift
-                                MONGODBHOST=$1                      
+                                MONGODBHOST=$1  
+                                ;;                    
         -h | --help )           usage
                                 exit
                                 ;;
@@ -36,6 +47,12 @@ if [ ! -d "$WLP_BASE_DIR" ]; then
  echo "Liberty not installed in $WLP_BASE_DIR...Exiting script." 
  exit 1
 fi
+
+#Check if the sample is already installed, if so clean it up before reinstalling
+if [ -d "$WLP_BASE_DIR/wlp/usr/servers/mongoDBSample" ]; then
+	cleanup
+fi
+
 yum -y install screen
 #Install the sample app and its dependencies
 cd $WLP_BASE_DIR
